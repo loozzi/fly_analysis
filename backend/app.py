f7540ba8615cfe185604310a968061ba296d24c4
Nguyen Quang Thao <nquangthao19072004@gmail.com>
2024-05-29 09:23:49 +0700

    add Dockerfile

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
 from src import app
 
 if __name__ == "__main__":
-    app.run(debug=True, port=1000)
+    app.run(debug=True, port=1000, host="0.0.0.0")
--- backend/src/services/file_service.py
+++ backend/src/services/file_service.py
@@ -7,7 +7,14 @@ class FileService:
     def __init__(self):
         pass
 
+    def check_folder_exists(self, folder_path: str):
+        import os
+
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+
     def save_file(self, file: any):
+        self.check_folder_exists("./src/uploads")
         file_name = f"./src/uploads/{randint(0, 100000)}_{file.filename}"
         open(file_name, "wb").write(file.read())
         return file_name
